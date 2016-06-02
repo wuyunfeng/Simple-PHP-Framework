575b3d927afc42fdd4a50f7136a2a2a2aaad4534
wuyunfeng01 <wuyunfeng01@baidu.com>
2016-06-02 13:37:55 +0800

    database configure file

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     'host' => '127.0.0.1',
     'port' => 3306,
     'user' => 'root',
-    'password' => '*',
+    'password' => '****',
     'database' => 'project',
     'charset' => 'utf-8'
 ];
