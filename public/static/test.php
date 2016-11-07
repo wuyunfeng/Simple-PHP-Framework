0fb05d2ca460be28317ba7d0fd2026119f5e7d14
wuyunfeng01 <wuyunfeng01@baidu.com>
2016-11-07 19:52:08 +0800

    add: add some static resource

---
+++ public/static/test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <style rel="stylesheet" href=""></style>
+    <link href="../css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="../css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+</head>
+<body>
+<p class="main_p">测试路径问题</p>
+
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="../js/jquery-2.0.3.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
